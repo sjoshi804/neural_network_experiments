f23035f88532902ec8bffc6aed510456ec336b87
Siddharth Joshi <sjoshi804@gmail.com>
2021-10-21 22:15:34 -0700

    added support for mnist

--- neural_network_experiments/lib/util.py
+++ neural_network_experiments/lib/util.py
@@ -6,25 +6,49 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
 
 def get_data(dataset: str):
-    training_data = datasets.CIFAR10(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor(),
-        # target_transform=Lambda(
-        #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
-        # )
-    )
-    test_data = datasets.CIFAR10(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor(),
-        # target_transform=Lambda(
-        #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
-        # )
-    )
-    return training_data, test_data
+    if dataset == "CIFAR10":
+        training_data = datasets.CIFAR10(
+            root="data",
+            train=True,
+            download=True,
+            transform=ToTensor(),
+            # target_transform=Lambda(
+            #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
+            # )
+        )
+        test_data = datasets.CIFAR10(
+            root="data",
+            train=False,
+            download=True,
+            transform=ToTensor(),
+            # target_transform=Lambda(
+            #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
+            # )
+        )
+        return training_data, test_data
+
+    elif dataset == "MNIST":
+        training_data = datasets.MNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=ToTensor(),
+            # target_transform=Lambda(
+            #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
+            # )
+        )
+        test_data = datasets.MNIST(
+            root="data",
+            train=False,
+            download=True,
+            transform=ToTensor(),
+            # target_transform=Lambda(
+            #     lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
+            # )
+        )
+        return training_data, test_data
+    else:
+        raise Exception("dataset not supported")
 
 def split_data_into_batches(data, batch_size):
     batched_data = []
@@ -66,11 +90,12 @@ def outer_product_tensor_w_vector(a_tensor: tensor, a_vector: tensor):
 
 def effective_rank(weight_matrix):
     _, singular_values, _ = torch.svd(weight_matrix)
-    normalized_singular_values = F.normalize(singular_values, 1.0)
+    sum = np.sum(np.array(singular_values))
+    normalized_singular_values = singular_values/sum
     return entropy(normalized_singular_values)
 
 def entropy(vector):
     prob = np.array(vector)
     log_prob = np.log2(prob)
-    return np.sum(-prob*log_prob)
+    return np.sum(-prob*log_prob)/np.log2(len(prob))
 
