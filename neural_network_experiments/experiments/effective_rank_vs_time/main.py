2907238fab062fa967f24962c3c0655f92b7423a
Siddharth Joshi <sjoshi804@gmail.com>
2021-10-27 23:41:45 -0700

    added plotting of test acc

--- neural_network_experiments/experiments/effective_rank_vs_time/main.py
+++ neural_network_experiments/experiments/effective_rank_vs_time/main.py
@@ -41,6 +41,7 @@ def test_loop(dataloader, model, loss_fn, device):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return (100*correct)
 
 
 def main(device: str):
@@ -61,11 +62,12 @@ def main(device: str):
     for layer in model.layers:
         if type(layer) is not nn.ReLU:
             ranks.append([])
-
+    
+    accuracies = []
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train_loop(train_dataloader, model, loss_fn, optimizer, device)
-        test_loop(test_dataloader, model, loss_fn, device)
+        accuracies.append(test_loop(test_dataloader, model, loss_fn, device))
 
         # Compute effective rank for each layer
         i = 0
@@ -92,6 +94,13 @@ def main(device: str):
     plt.ylabel("Effective Rank")
     plt.title("Effective Rank v/s Layers")
     plt.savefig("CIFAR-10 Layers Final Effective Ranks")
+    plt.clf()
+
+    plt.plot(range(epochs), accuracies)
+    plt.xlabel("Epochs")
+    plt.ylabel("Test Accuracy")
+    plt.title("Test Accuracy")
+    plt.savefig("CIFAR-10 Test Accuracy")
     plt.clf()
     print("Done!")
 
